8643b7e1502c71fb19777e5e5fe6b33def4e17db
matsbyfl <mats@byfuglien.net>
2020-05-11 18:02:08 +0200

    Use tempdirAdd spring session to test config aswell

--- src/test/java/no/nav/aura/envconfig/spring/SpringSecurityTestConfig.java
+++ src/test/java/no/nav/aura/envconfig/spring/SpringSecurityTestConfig.java
@@ -8,8 +8,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @Configuration
+@EnableJdbcHttpSession
 @ImportResource({ "classpath:spring-test-security.xml" })
 @Import(SpringSecurityHandlersConfig.class)
 public class SpringSecurityTestConfig {
