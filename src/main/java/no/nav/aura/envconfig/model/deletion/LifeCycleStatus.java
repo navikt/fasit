45bc64d4683298e86b8842472939f173d58e1146
matsbyfl <mats@byfuglien.net>
2020-05-13 13:57:59 +0200

    Remove unused method

--- src/main/java/no/nav/aura/envconfig/model/deletion/LifeCycleStatus.java
+++ src/main/java/no/nav/aura/envconfig/model/deletion/LifeCycleStatus.java
@@ -2,9 +2,4 @@ package no.nav.aura.envconfig.model.deletion;
 
 public enum LifeCycleStatus {
     ALERTED, STOPPED, RUNNING, RESCUED;
-
-    public static boolean isDeleteCandidate(LifeCycleStatus status) {
-        return STOPPED == status || ALERTED == status;
-    }
-
 }
