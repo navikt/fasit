b79f80dc5d0ed16ccdb7918da3d959675620524b
Håvard Tronhus <46346413+havtro@users.noreply.github.com>
2025-07-16 13:41:50 +0200

    cleanup

--- src/main/java/no/nav/aura/envconfig/spring/SpringDomainConfig.java
+++ src/main/java/no/nav/aura/envconfig/spring/SpringDomainConfig.java
@@ -140,6 +140,8 @@ public class SpringDomainConfig {
     CookieSerializer cookieSerializer() {
         DefaultCookieSerializer serializer = new DefaultCookieSerializer();
         serializer.setCookieName("JSESSIONID");
+        
+        // TODO better handling of SameSite attribute. Should be set to "Lax" by default or have a check for isDev. 
         serializer.setSameSite("None");  // Allow cross-site requests
         serializer.setUseSecureCookie(true);  // Required when SameSite=None
         return serializer;
