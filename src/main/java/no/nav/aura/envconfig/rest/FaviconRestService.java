79d8766c0da786aafae50b738586230beb690458
Håvard Tronhus <46346413+havtro@users.noreply.github.com>
2026-01-19 11:51:52 +0100

    multipart message fix

--- src/main/java/no/nav/aura/envconfig/rest/FaviconRestService.java
+++ src/main/java/no/nav/aura/envconfig/rest/FaviconRestService.java
@@ -1,5 +1,6 @@
 package no.nav.aura.envconfig.rest;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,13 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 /** Unngå notfound execption når restapi brukes med browser */
 @RestController
-@RequestMapping(path = "/conf/favicon.ico")
+@RequestMapping(path = "/favicon.ico")
 public class FaviconRestService {
 
     /**
      * Dummytjeneste for å unngå notfound når restapi brukes av en browser
      */
     @GetMapping
-    public void favico() {
+    public ResponseEntity<Object> favico() {
+    	return ResponseEntity.noContent().build();
     }
 }
